5d11eb5fe5c72170f60beec2ddbd39c6325d596c
Ali Messaoudi <83890004+MESSDEV@users.noreply.github.com>
2023-04-11 04:47:58 +0200

    Update formoid.php

--- IT-School/formoid.php
+++ IT-School/formoid.php
@@ -1,10 +1,10 @@
 <?php $form_path='formoid_files/formoid1/form.php'; require_once $form_path; ?><!DOCTYPE html>
 <html>
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<!--head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 	<title> - Formoid form builder</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
+</head-->
 <body class="blurBg-false" style="background-color:#EBEBEB">
 
 {{Formoid}}
